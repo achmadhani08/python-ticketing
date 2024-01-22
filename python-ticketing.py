cf0f0f2aa3fcbe5337e8bc6fa1011484f1d9014c
achmadhani08 <achmadhani08@gmail.com>
2024-01-22 18:07:57 +0700

    Add dynamic function for user input departure & destination route

--- python-ticketing.py
+++ python-ticketing.py
@@ -1,12 +1,13 @@
 import locale
 from prettytable import PrettyTable
 
+# set locale for currency format number
 locale.setlocale(locale.LC_ALL, "id_ID")
 
 # List kota
 list_kota = ["Jakarta", "Malang", "Bandung", "Yogyakarta", "Surabaya"]
 
-# List ticket Jakarta
+# Pricelist Ticket Jakarta
 jkt_ticket_pricelist = {
     "Jakarta - Malang": {
         "train": {
@@ -50,7 +51,7 @@ jkt_ticket_pricelist = {
     },
 }
 
-# List ticket Malang
+# Pricelist Ticket Malang
 mlg_ticket_pricelist = {
     "Malang - Jakarta": {
         "train": {
@@ -94,7 +95,7 @@ mlg_ticket_pricelist = {
     },
 }
 
-# List ticket Bandung
+# Pricelist Ticket Bandung
 bdg_ticket_pricelist = {
     "Bandung - Jakarta": {
         "train": {
@@ -138,7 +139,7 @@ bdg_ticket_pricelist = {
     },
 }
 
-# List ticket Yogyakarta
+# Pricelist Ticket Yogyakarta
 ygy_ticket_pricelist = {
     "Yogyakarta - Malang": {
         "train": {
@@ -182,7 +183,7 @@ ygy_ticket_pricelist = {
     },
 }
 
-# List ticket Surabaya
+# Pricelist Ticket Surabaya
 sby_ticket_pricelist = {
     "Surabaya - Malang": {
         "train": {
@@ -226,9 +227,20 @@ sby_ticket_pricelist = {
     },
 }
 
+# Pricelist Hotels
+hotel_pricelist = {
+    "Jakarta": {"weekday": 0000, "weekend": 0000},
+    "Malang": {"weekday": 0000, "weekend": 0000},
+    "Bandung": {"weekday": 0000, "weekend": 0000},
+    "Yogyakarta": {"weekday": 0000, "weekend": 0000},
+    "Surabaya": {"weekday": 0000, "weekend": 0000},
+}
 
 # Header Table City
 header_table_city = ["No", "Daftar Kota Keberangkatan/Tujuan"]
+
+# Header Table Hotel
+header_table_hotel = ["Kota", "Weekday/malam", "Weekend/malam"]
 
 # Header Table Route
 header_table = [
@@ -249,25 +261,27 @@ for route in jkt_ticket_pricelist.keys():
     routes.append(route)
 
 # Create a PrettyTable instance
-table_list_city = PrettyTable()
+table_list_city = PrettyTable()  # city
+table_list_hotel = PrettyTable()  # hotel
 
-pricelist_route1 = PrettyTable()
-pricelist_route2 = PrettyTable()
-pricelist_route3 = PrettyTable()
-pricelist_route4 = PrettyTable()
+pricelist_jkt1 = PrettyTable()
+pricelist_jkt2 = PrettyTable()
+pricelist_jkt3 = PrettyTable()
+pricelist_jkt4 = PrettyTable()
 
 # Set the header table
 table_list_city.field_names = header_table_city  # Header for City Table
+table_list_hotel.field_names = header_table_hotel  # Header for Hotel Table
 
-pricelist_route1.field_names = (
-    pricelist_route2.field_names
-) = pricelist_route3.field_names = pricelist_route4.field_names = header_table
+pricelist_jkt1.field_names = (
+    pricelist_jkt2.field_names
+) = pricelist_jkt3.field_names = pricelist_jkt4.field_names = header_table
 
 # Set the alignment of columns
 table_list_city.align = "l"
-pricelist_route1.align = (
-    pricelist_route2.align
-) = pricelist_route3.align = pricelist_route4.align = "c"
+pricelist_jkt1.align = (
+    pricelist_jkt2.align
+) = pricelist_jkt3.align = pricelist_jkt4.align = "c"
 
 # Adding row table city list
 table_list_city.add_rows(
@@ -281,7 +295,7 @@ table_list_city.add_rows(
 )
 
 # Adding row table route 1: Jakarta - Malang
-pricelist_route1.add_rows(
+pricelist_jkt1.add_rows(
     [
         [
             routes[0],
@@ -335,7 +349,7 @@ pricelist_route1.add_rows(
 )
 
 # Adding row table route 2: Jakarta - Bandung
-pricelist_route2.add_rows(
+pricelist_jkt2.add_rows(
     [
         [
             routes[1],
@@ -389,7 +403,7 @@ pricelist_route2.add_rows(
 )
 
 # Adding row table route 3: Jakarta - Yogyakarta
-pricelist_route3.add_rows(
+pricelist_jkt3.add_rows(
     [
         [
             routes[2],
@@ -443,7 +457,7 @@ pricelist_route3.add_rows(
 )
 
 # Adding row table route 4: Jakarta - Surabaya
-pricelist_route4.add_rows(
+pricelist_jkt4.add_rows(
     [
         [
             routes[3],
@@ -498,27 +512,97 @@ pricelist_route4.add_rows(
 
 
 # Print the table
-def showPriceList(route):
-    if route == "1":
-        print(pricelist_route1)
-    elif route == "2":
-        print(pricelist_route2)
-    elif route == "3":
-        print(pricelist_route3)
-    elif route == "4":
-        print(pricelist_route4)
+def showPriceList(
+    route_number,
+    pricelist_route_1,
+    pricelist_route_2,
+    pricelist_route_3,
+    pricelist_route_4,
+):
+    if route_number == "1":
+        print(pricelist_route_1)
+    elif route_number == "2":
+        print(pricelist_route_2)
+    elif route_number == "3":
+        print(pricelist_route_3)
+    elif route_number == "4":
+        print(pricelist_route_4)
 
 
 # Show List City
 print(table_list_city)
 
 # Input Departure Route
-departure_route = str(input("Masukkan kota keberangkatan "))
+departure_route = int(input("Masukkan nomor kota keberangkatan "))
 print("Anda dari:", departure_route)
 
 # Inpute Destination Route
-destination_route = str(input("Masukkan kota tujuan "))
+destination_route = int(input("Masukkan nomor kota tujuan "))
 print("Anda menuju:", destination_route)
 
+
+# select pricelist by input user
+def selectPriceList():
+    if departure_route == 1:
+        if destination_route == 2:
+            showPriceList(
+                "1", pricelist_jkt1, pricelist_jkt2, pricelist_jkt3, pricelist_jkt4
+            )
+        elif destination_route == 3:
+            showPriceList(
+                "2", pricelist_jkt1, pricelist_jkt2, pricelist_jkt3, pricelist_jkt4
+            )
+        elif destination_route == 4:
+            showPriceList(
+                "3", pricelist_jkt1, pricelist_jkt2, pricelist_jkt3, pricelist_jkt4
+            )
+        elif destination_route == 5:
+            showPriceList(
+                "4", pricelist_jkt1, pricelist_jkt2, pricelist_jkt3, pricelist_jkt4
+            )
+        else:
+            print(
+                "Mohon masukkan input kota tujuan yang berbeda dengan kota keberangkatan!"
+            )
+    # elif (departure_route == 2):
+    #     if (destination_route == 1):
+    #         showPriceList("1", pricelist_mlg1, pricelist_mlg2, pricelist_mlg3, pricelist_mlg4)
+    #     elif (destination_route == 3):
+    #         showPriceList("2", pricelist_mlg1, pricelist_mlg2, pricelist_mlg3, pricelist_mlg4)
+    #     elif (destination_route == 4):
+    #         showPriceList("3", pricelist_mlg1, pricelist_mlg2, pricelist_mlg3, pricelist_mlg4)
+    #     elif (destination_route == 5):
+    #         showPriceList("4", pricelist_mlg1, pricelist_mlg2, pricelist_mlg3, pricelist_mlg4)
+    # elif (departure_route == 3):
+    #     if (destination_route == 1):
+    #         showPriceList("1", pricelist_bdg1, pricelist_bdg2, pricelist_bdg3, pricelist_bdg4)
+    #     elif (destination_route == 2):
+    #         showPriceList("2", pricelist_bdg1, pricelist_bdg2, pricelist_bdg3, pricelist_bdg4)
+    #     elif (destination_route == 4):
+    #         showPriceList("3", pricelist_bdg1, pricelist_bdg2, pricelist_bdg3, pricelist_bdg4)
+    #     elif (destination_route == 5):
+    #         showPriceList("4", pricelist_bdg1, pricelist_bdg2, pricelist_bdg3, pricelist_bdg4)
+    # elif (departure_route == 4):
+    #     if (destination_route == 1):
+    #         showPriceList("1", pricelist_ygy1, pricelist_ygy2, pricelist_ygy3, pricelist_ygy4)
+    #     elif (destination_route == 2):
+    #         showPriceList("2", pricelist_ygy1, pricelist_ygy2, pricelist_ygy3, pricelist_ygy4)
+    #     elif (destination_route == 3):
+    #         showPriceList("3", pricelist_ygy1, pricelist_ygy2, pricelist_ygy3, pricelist_ygy4)
+    #     elif (destination_route == 5):
+    #         showPriceList("4", pricelist_ygy1, pricelist_ygy2, pricelist_ygy3, pricelist_ygy4)
+    # elif (departure_route == 5):
+    #     if (destination_route == 1):
+    #         showPriceList("1", pricelist_sby1, pricelist_sby2, pricelist_sby3, pricelist_sby4)
+    #     elif (destination_route == 2):
+    #         showPriceList("2", pricelist_sby1, pricelist_sby2, pricelist_sby3, pricelist_sby4)
+    #     elif (destination_route == 3):
+    #         showPriceList("3", pricelist_sby1, pricelist_sby2, pricelist_sby3, pricelist_sby4)
+    #     elif (destination_route == 4):
+    #         showPriceList("4", pricelist_sby1, pricelist_sby2, pricelist_sby3, pricelist_sby4)
+
+
 # Show table
-# showPriceList("4")
+# showPriceList("4", pricelist_jkt1, pricelist_jkt2, pricelist_jkt3, pricelist_jkt4)
+
+selectPriceList()
